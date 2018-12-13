3a857e5b11391fc23151bac7bdfa75c7247e70be
Kiselev <j8kiselev@gmail.com>
2018-12-13 21:33:10 +0300

    Configuration app with spring

---
+++ src/main/java/ru/icoltd/springdemo/config/AppConfig.java
@@ -0,0 +1,11 @@
+package ru.icoltd.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.icoltd.springdemo")
+public class AppConfig {
+}
