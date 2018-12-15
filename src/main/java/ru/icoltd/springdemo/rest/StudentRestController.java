afd534a2fc0b33eb28ac46ed95c5f6d89c52e11b
Kiselev <j8kiselev@gmail.com>
2018-12-15 15:34:33 +0300

    Adding @ControllerAdvice for support global exception handler (best practice)

--- src/main/java/ru/icoltd/springdemo/rest/StudentRestController.java
+++ src/main/java/ru/icoltd/springdemo/rest/StudentRestController.java
@@ -41,34 +41,4 @@ public class StudentRestController {
         }
         return theStudents.get(studentId);
     }
-
-    // add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> exceptionHandler(StudentNotFoundException exc) {
-
-        // create a StudentErrorResponse
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setMessage(exc.getMessage());
-        errorResponse.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<StudentErrorResponse>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler to catch any exception
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> anyExceptionHandler(Exception exc) {
-
-        // create a StudentErrorResponse
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setMessage(exc.getMessage());
-        errorResponse.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-        return new ResponseEntity<StudentErrorResponse>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
 }
